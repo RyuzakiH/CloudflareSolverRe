1b2297b8947b5b756be27814ed5808c0244030e0
RyuzakiH <hazemahmedamr.zero@gmail.com>
2019-07-12 11:50:11 +0200

    Added captcha testing

--- test/CloudflareSolverRe.Tests/WebsiteChallengeTests.cs
+++ test/CloudflareSolverRe.Tests/WebsiteChallengeTests.cs
@@ -1,3 +1,4 @@
+using CloudflareSolverRe.CaptchaProviders;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Net.Http;
@@ -18,6 +19,46 @@ namespace CloudflareSolverRe.Tests
             var uri = new Uri("https://uam.hitmehard.fun/HIT");
 
             var result = cf.Solve(httpClient, httpClientHandler, uri, 3).Result;
+
+            Assert.IsTrue(result.Success);
+
+            var response = httpClient.GetAsync(uri).Result;
+            var html = response.Content.ReadAsStringAsync().Result;
+
+            Assert.AreEqual("Dstat.cc is the best", html);
+        }
+
+        [TestMethod]
+        public void SolveWebsiteChallenge_uamhitmehardfun_WithAntiCaptcha()
+        {
+            var cf = new CloudflareSolver(new AntiCaptchaProvider("YOUR_API_KEY"));
+
+            var httpClientHandler = new HttpClientHandler();
+            var httpClient = new HttpClient(httpClientHandler);
+
+            var uri = new Uri("https://uam.hitmehard.fun/HIT");
+
+            var result = cf.Solve(httpClient, httpClientHandler, uri, 1).Result;
+
+            Assert.IsTrue(result.Success);
+
+            var response = httpClient.GetAsync(uri).Result;
+            var html = response.Content.ReadAsStringAsync().Result;
+
+            Assert.AreEqual("Dstat.cc is the best", html);
+        }
+
+        [TestMethod]
+        public void SolveWebsiteChallenge_uamhitmehardfun_With2Captcha()
+        {
+            var cf = new CloudflareSolver(new TwoCaptchaProvider("YOUR_API_KEY"));
+
+            var httpClientHandler = new HttpClientHandler();
+            var httpClient = new HttpClient(httpClientHandler);
+
+            var uri = new Uri("https://uam.hitmehard.fun/HIT");
+
+            var result = cf.Solve(httpClient, httpClientHandler, uri, 1).Result;
 
             Assert.IsTrue(result.Success);
 
