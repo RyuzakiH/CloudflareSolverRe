a53e16f8f94d6e2242ee907e19439546224102a5
RyuzakiH <hazemahmedamr.zero@gmail.com>
2019-07-18 22:37:47 +0200

    Updated nuget info

--- sample/CloudflareSolverRe.Sample/Program.cs
+++ sample/CloudflareSolverRe.Sample/Program.cs
@@ -12,15 +12,7 @@
             ClearanceHandlerSample.Sample();
 
             CloudflareSolverSample.Sample();
-            
-            //var uri = new Uri("https://www.japscan.to");
-            //var uri = new Uri("https://www.spacetorrent.cloud/");
-            //var uri = new Uri("https://hdmovie8.com");
-            //var uri = new Uri("https://github.com");
-            //var uri = new Uri("https://www.mkvcage.ws/");
-            //var uri = new Uri("https://codepen.io/");
-            //var uri = new Uri("https://uam.hitmehard.fun/HIT");
-            
+                        
         }
     }
 }
--- src/CloudflareSolverRe/build.bat
+++ src/CloudflareSolverRe/build.bat
@@ -1,3 +1,2 @@
 @echo off
 dotnet publish -c Release -f netstandard1.1
-::dotnet publish -c Release -f netstandard2.0
