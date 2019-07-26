9b1e584d6ed2129ed04e8f4c55c0788dd18d07a8
RyuzakiH <hazemahmedamr.zero@gmail.com>
2019-07-26 05:45:07 +0200

    Updated nuget package version to 1.0.2

--- sample/CloudflareSolverRe.Sample/Program.cs
+++ sample/CloudflareSolverRe.Sample/Program.cs
@@ -4,15 +4,9 @@
     {
         static void Main(string[] args)
         {
-            /* Example use with captcha provider:
-             * var cf = new CloudflareSolver(new TwoCaptchaProvider("YOUR_API_KEY"));
-             * var cf = new CloudflareSolver(new AntiCaptchaProvider("YOUR_API_KEY"));
-             */
-
             ClearanceHandlerSample.Sample();
 
             CloudflareSolverSample.Sample();
-                        
         }
     }
 }
